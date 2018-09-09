3df8509cb5237713f628ed0ba88c535e88ddc713
Johanne <johannegi16@ru.is>
2018-09-09 17:56:07 +0000

    hasda

--- scanner.cpp
+++ scanner.cpp
@@ -33,43 +33,43 @@ std::vector<std::string> PORTS;
 
 void error(const char *msg)
 {
-    perror(msg);
+	perror(msg);
 }
 
 struct pseudo_header
 {
-    u_int32_t source_address;
-    u_int32_t dest_address;
-    u_int8_t placeholder;
-    u_int8_t protocol;
-    u_int16_t tcp_length;
+	u_int32_t source_address;
+	u_int32_t dest_address;
+	u_int8_t placeholder;
+	u_int8_t protocol;
+	u_int16_t tcp_length;
 };
- 
+
 /*
     Generic checksum calculation function
 */
 unsigned short csum(unsigned short *ptr,int nbytes) 
 {
-    register long sum;
-    unsigned short oddbyte;
-    register short answer;
- 
-    sum=0;
-    while(nbytes>1) {
-        sum+=*ptr++;
-        nbytes-=2;
-    }
-    if(nbytes==1) {
-        oddbyte=0;
-        *((u_char*)&oddbyte)=*(u_char*)ptr;
-        sum+=oddbyte;
-    }
- 
-    sum = (sum>>16)+(sum & 0xffff);
-    sum = sum + (sum>>16);
-    answer=(short)~sum;
-     
-    return(answer);
+	register long sum;
+	unsigned short oddbyte;
+	register short answer;
+	
+	sum=0;
+	while(nbytes>1) {
+		sum+=*ptr++;
+		nbytes-=2;
+	}
+	if(nbytes==1) {
+		oddbyte=0;
+		*((u_char*)&oddbyte)=*(u_char*)ptr;
+		sum+=oddbyte;
+	}
+	
+	sum = (sum>>16)+(sum & 0xffff);
+	sum = sum + (sum>>16);
+	answer=(short)~sum;
+	
+	return(answer);
 }
 
 std::vector<std::string> getPorts(std::string s)
@@ -80,14 +80,14 @@ std::vector<std::string> getPorts(std::string s)
 
 	std::ifstream inPorts(portFile.c_str());
 	std::string tmpLine;
- 
+	
 	// Check if object is valid
 	if(!inPorts)
 	{
 		std::cerr << "Cannot open the File : "<<portFile<<std::endl;
 		exit(1);
 	}
- 
+	
 	
 	// Read the next line from File untill it reaches the end.
 	while (std::getline(inPorts, tmpLine))
@@ -98,12 +98,12 @@ std::vector<std::string> getPorts(std::string s)
 	}
 	//Close The File
 	inPorts.close();
-		
+	
 	//shuffle the vector so the PORTS will be tested in random order
 	unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
 	std::default_random_engine rng (seed);
 	std::shuffle(PORTS.begin(), PORTS.end(), rng);
-    return PORTS;
+	return PORTS;
 }
 
 std::vector<std::string> getHosts(std::string s)
@@ -115,14 +115,14 @@ std::vector<std::string> getHosts(std::string s)
 
 	std::ifstream inHosts(hostFile.c_str());
 	std::string tmpLine;
- 
+	
 	// Check if object is valid
 	if(!inHosts)
 	{
 		std::cerr << "Cannot open the File : "<<hostFile<<std::endl;
 		exit(1);
 	}
- 
+	
 	// Read the next line from File untill it reaches the end.
 	while (std::getline(inHosts, tmpLine))
 	{
@@ -132,15 +132,15 @@ std::vector<std::string> getHosts(std::string s)
 	}
 	//Close The File
 	inHosts.close();
-    return HOSTS;
+	return HOSTS;
 }
 
 void createIp(iphdr *iph, char *source_ip, sockaddr_in &sin, char *datagram)
 {
 	iph->ihl = 5;
-    iph->version = 4;
-    iph->tos = 0;
-    iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr);
+	iph->version = 4;
+	iph->tos = 0;
+	iph->tot_len = sizeof (struct iphdr) + sizeof (struct tcphdr);
     iph->id = htonl (54321); //Id of this packet
     iph->frag_off = 0;
     iph->ttl = 255;
@@ -148,7 +148,7 @@ void createIp(iphdr *iph, char *source_ip, sockaddr_in &sin, char *datagram)
     iph->check = 0;      //Set to 0 before calculating checksum
     iph->saddr = inet_addr ( source_ip );    //Spoof the source ip address
     iph->daddr = sin.sin_addr.s_addr;
-     
+    
     //Ip checksum
     iph->check = csum ((unsigned short *) datagram, iph->tot_len);
 }
@@ -156,10 +156,10 @@ void createIp(iphdr *iph, char *source_ip, sockaddr_in &sin, char *datagram)
 void createTcp(tcphdr *tcph, int portNo)
 {
 	//TCP Header
-    tcph->source = htons (1234);
-    tcph->dest = htons (portNo);
-    tcph->seq = 0;
-    tcph->ack_seq = 0;
+	tcph->source = htons (1234);
+	tcph->dest = htons (portNo);
+	tcph->seq = 0;
+	tcph->ack_seq = 0;
     tcph->doff = 5;  //tcp header size
     tcph->fin=FIN;
     tcph->syn=SYN;
@@ -200,30 +200,30 @@ void scanIP(int i)
 		{
 			error("Failed to create socket");
 		}
-			
+		
 		//Datagram to represent the packet
 		char datagram[4096] , source_ip[32], *pseudogram;
 		memset (datagram, 0, 4096);
-			
+		
 		//IP header
 		struct iphdr *write_iphdr = (struct iphdr *) datagram;
-			
+		
 		//TCP header
 		struct tcphdr *write_tcphdr = (struct tcphdr *) (datagram + sizeof (struct ip));
 		struct sockaddr_in sin;
 		struct pseudo_header psh;
 		
-			
+		
 		//some address resolution
 		strcpy(source_ip , USERIP.c_str());
 		sin.sin_family = AF_INET;
 		//sin.sin_addr.s_addr = inet_addr (HOSTS[i].c_str());
 		bcopy((char *)addrs.c_str(), 
-				(char *)&sin.sin_addr.s_addr,
-				addrslength);
+			(char *)&sin.sin_addr.s_addr,
+			addrslength);
 
 		sin.sin_port = htons(stoi(PORTS[x]));
-			
+		
 		//Fill in the IP and TCP Header
 		createIp(write_iphdr, source_ip, sin, datagram);
 		createTcp(write_tcphdr, portNo);
@@ -234,19 +234,19 @@ void scanIP(int i)
 		psh.placeholder = 0;
 		psh.protocol = IPPROTO_TCP;
 		psh.tcp_length = htons(sizeof(struct tcphdr));
-			
+		
 		int psize = sizeof(struct pseudo_header) + sizeof(struct tcphdr);
 		pseudogram = (char*)malloc(psize);
-			
+		
 		memcpy(pseudogram , (char*) &psh , sizeof (struct pseudo_header));
 		memcpy(pseudogram + sizeof(struct pseudo_header) , write_tcphdr , sizeof(struct tcphdr));
-			
+		
 		write_tcphdr->check = csum( (unsigned short*) pseudogram , psize);
-			
+		
 		//IP_HDRINCL to tell the kernel that headers are included in the packet
 		int one = 1;
 		const int *val = &one;
-			
+		
 		if (setsockopt (write_socket, IPPROTO_IP, IP_HDRINCL, val, sizeof (one)) < 0)
 		{
 			error("Error setting IP_HDRINCL");
@@ -254,7 +254,7 @@ void scanIP(int i)
 		struct timeval tv;
 		tv.tv_sec = 10;
 		setsockopt (read_socket, SOL_SOCKET, SO_RCVTIMEO, (struct timeval *)&tv,sizeof(struct timeval)) != 0;
-			
+		
 		//Send the packet
 		if (sendto (write_socket, datagram, write_iphdr->tot_len ,  0, (struct sockaddr *) &sin, sizeof (sin)) < 0)
 		{
@@ -285,11 +285,11 @@ void scanIP(int i)
 				break;
 			}
 			duration = ( std::chrono::steady_clock::now() - start ) /  std::chrono::milliseconds(1);
-		} while(duration < 350);
+		} while(duration < 500);
 
 		if( received_bytes < 0 )
 		{
-				printf("Port: %d timed out at host: %s\n", portNo, HOSTS[i].c_str());
+			printf("Port: %d timed out at host: %s\n", portNo, HOSTS[i].c_str());
 				//error("\n\t timed out \n");
 				//break;
 		}
@@ -339,68 +339,68 @@ void scan()
 		tasks.push_back(new std::thread(scanIP, i));
 	}
 	for (int i=0; i<HOSTS.size(); i++)
-    {
-        tasks[i]->join();
-        delete tasks[i];
-    }
+	{
+		tasks[i]->join();
+		delete tasks[i];
+	}
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc < 5) {
-       fprintf(stderr,"usage %s (your ip address) hosts.txt ports.txt FLAG(S = SYN, F = FIN, N = null, X = xmas)\n", argv[0]);
-       fprintf("Example ./scanner 192.168.1.1 hosts.txt ports.txt S\n");
-       exit(0);
-    }
+	if (argc < 5) {
+		fprintf(stderr,"usage %s (your ip address) hosts.txt ports.txt FLAG(S = SYN, F = FIN, N = null, X = xmas)\n", argv[0]);
+		fprintf(stderr,"Example ./scanner 192.168.1.1 hosts.txt ports.txt S\n");
+		exit(0);
+	}
 
-    USERIP = argv[1];
-    std::string hostsFile = argv[2];
-    std::string portsFile = argv[3];
-    FLAG = argv[4];
-    HOSTS = getHosts(hostsFile);
-    PORTS = getPorts(portsFile);
+	USERIP = argv[1];
+	std::string hostsFile = argv[2];
+	std::string portsFile = argv[3];
+	FLAG = argv[4];
+	HOSTS = getHosts(hostsFile);
+	PORTS = getPorts(portsFile);
 
-    if (FLAG == "S")
-    {
+	if (FLAG == "S")
+	{
 		SYN = 1;
 		FIN = 0;
 		PUSH = 0;
 		URG = 0;
-        scan();
-    }
-    else if (FLAG == "F")
-    {
+		scan();
+	}
+	else if (FLAG == "F")
+	{
 		SYN = 0;
 		FIN = 1;
 		PUSH = 0;
 		URG = 0;
-        scan();
-        
-    }
-    else if (FLAG == "N")
-    {
+		scan();
+		
+	}
+	else if (FLAG == "N")
+	{
 		SYN = 0;
 		FIN = 0;
 		PUSH = 0;
 		URG = 0;
-        scan();
-        
-    }
-    else if (FLAG == "X")
-    {
+		scan();
+		
+	}
+	else if (FLAG == "X")
+	{
 		SYN = 0;
 		FIN = 1;
 		PUSH = 1;
 		URG = 1;
-        scan();
-        
-    }
-    else
+		scan();
+		
+	}
+	else
 	{
-        printf("error\n");
-    }
+		printf("error\n");
+	}
 
 	
 
-    return 0;
+	return 0;
 }
